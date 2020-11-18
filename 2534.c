de3f96ebbe8db56d8231a4df3e8b0947c10cf1d5
Senhor Bento <senhorbento@users.noreply.github.com>
2020-11-18 07:57:25 -0300

    .

--- 2534.c
+++ 2534.c
@@ -9,21 +9,24 @@ void lerVetor(int notas[], int n){
 		scanf("%d",&notas[i]);
 }
 
-int compint(){
-	return ;
+int compint (const void * a, const void * b)
+{
+    return *(int*)b-*(int*)a;
 }
 
 int main()
 {
-	int n,q,pos;	
+	int n,q,pos,i;	
 	int notas[200];
 	while ( scanf("%d %d",&n, &q)==2 ){
 		lerVetor(notas,n);
                 //a funcao qsort ja existe no c
                 //vc so precisa criar a compint (se vira malandro)
 		qsort(notas,n, sizeof(int), compint); 
-		scanf("%d",&pos);
-		printf("%d\n",notas[pos-1]);
+		for ( i=0; i<=q - 1; i=i+1 ){
+			scanf("%d",&pos);
+			printf("%d\n",notas[pos-1]);
+		}
 	}
     return 0;
 }
